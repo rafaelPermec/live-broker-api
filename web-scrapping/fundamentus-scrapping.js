09f723b5246a682dbe71905666ceaac13f632a13
rafaelPermec <geekchef.dev@gmail.com>
2022-07-17 04:24:46 -0300

    :mag: (Web-scrapping) pool: Estruturando diretorios e apontando url do cheerios para a webpage da funtadamentus

---
+++ web-scrapping/fundamentus-scrapping.js
@@ -0,0 +1,6 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+// URL of the page we want to scrape
+const url = "https://www.fundamentus.com.br/resultado.php/api";
