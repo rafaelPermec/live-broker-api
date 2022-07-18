4c5bb69d9b8214ed6308fc3735278d1e6e482e09
rafaelPermec <geekchef.dev@gmail.com>
2022-07-18 12:24:05 -0300

    :surfer: (Pipeline) ci: fix: atualizando repo com actions certificadas

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import helmet from 'helmet';
 
 const app = express();
 
+app.use(helmet());
+
 app.get('/', (_request, response) => response.json({ message: 'Meu server Express, Typescript e ESLint! ' }));
 
-app.listen(3000, () => {
-  console.log('Back-end started in 3000 port!');
-});
+app.listen(3000, () => console.log('Back-end started in 3000 port!'));
