8758d378790213d23efedb63e2646b2b361df7c4
rafaelPermec <geekchef.dev@gmail.com>
2022-07-18 11:15:08 -0300

    :surfer: (Pipeline) ci: fix: Corrigindo github actions

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ app.get('/', (request, response) =>
   response.json({ message: 'Meu server Express, Typescript e ESLint! ' }),
 );
 
-app.listen(3333, () => {
-  console.log('Back-end started in 3333 port!');
+app.listen(3000, () => {
+  console.log('Back-end started in 3000 port!');
 }
 );
