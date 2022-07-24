660d9d7f1686c89247656588c4531111240b8a71
rafaelPermec <geekchef.dev@gmail.com>
2022-07-24 20:56:32 -0300

    :surfer: (Pipeline) ci: feat: Documentação do Swagger pronta para entrega!

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import 'express-async-errors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
 import { ErrorMiddleware, ValidationMiddleware } from './middlewares';
 import { ContasRoutes, LoginRoutes, AtivosRoutes, InvestimentosRoutes } from './routes';
+import swaggerDocs from './database/swagger.json';
 
 dotenv.config();
 
@@ -13,6 +15,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(express.json());
+
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/login', LoginRoutes);
 app.use('/conta', ContasRoutes);
